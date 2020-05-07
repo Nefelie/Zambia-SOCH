b498b763b42f0d47e041e8a8c655dd75bba80704
Nefelie <nefelie.hemrich@gmail.com>
2020-05-07 22:03:24 +0200

    navbar with dropdown menu

--- js/main.js
+++ js/main.js
@@ -287,3 +287,22 @@
 
 })(jQuery);
 
+/* dropdown*/
+
+$('.dropdown').hover(
+	function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+	}, 
+	function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+	}
+);
+
+$('.dropdown-menu').hover(
+	function() {
+		$(this).stop(true, true);
+	},
+	function() {
+		$(this).stop(true, true).delay(200).fadeOut();
+	}
+);
