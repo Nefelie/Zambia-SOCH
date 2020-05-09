1eb5e42843f80b2d9c9513c4fbf88dd70663332f
Nefelie <nefelie.hemrich@gmail.com>
2020-05-09 17:07:37 +0200

    removed dropdown and renamed news pages

--- js/main.js
+++ js/main.js
@@ -287,23 +287,4 @@
 
 })(jQuery);
 
-/* dropdown*/
-if (window.matchMedia("(min-width: 991.98px)").matches) {
-$('.dropdown').hover(
-	function() {
-		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
-	}, 
-	function() {
-		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-	}
-);
 
-$('.dropdown-menu').hover(
-	function() {
-		$(this).stop(true, true);
-	},
-	function() {
-		$(this).stop(true, true).delay(200).fadeOut();
-	}
-);
-}
