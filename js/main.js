54aa272e69d2c3813aa1282587b8addffba67a12
Nefelie <nefelie.hemrich@gmail.com>
2020-05-09 13:38:03 +0200

    updates to index page and other stuff

--- js/main.js
+++ js/main.js
@@ -307,12 +307,3 @@ $('.dropdown-menu').hover(
 	}
 );
 }
-
-//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
-
-/*
-if (!("ontouchstart" in document.documentElement)) {
-	document.documentElement.className += " no-touch";
-	}
-
-*/
